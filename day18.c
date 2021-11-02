8dfe45315a704abb232ad22c60472d077da59e6c
shreyas023 <91934591+shreyas023@users.noreply.github.com>
2021-11-02 15:24:37 +0530

    Create day18.c

---
+++ day18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int w;
+    printf("Weight at 18 : ");
+    scanf("%d",&w);
+    printf("\nWeight at 23 : %d",w+10);
+    printf("\nWeight at 28 : %d",w+20);
+    printf("\nWeight at 33 : %d",w+30);
+    printf("\nWeight at 38 : %d",w+20);
+    printf("\nWeight at 43 : %d",w+10);
+    return 0;
+}
